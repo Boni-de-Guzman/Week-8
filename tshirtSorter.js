83a6ae526b29abe289568c7332979cc62d1c8836
Boni De Guzman <boni.deg@gmail.com>
2022-04-14 21:26:38 -0700

    T-Shirt Sorter

---
+++ tshirtSorter.js
@@ -0,0 +1,18 @@
+function tshirtSorter(str) {
+  let small = ""
+  let medium = ""
+  let large = ""
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "s") {
+      small += "s"
+    } else if (str[i] === "m") {
+      medium += "m"
+    } else {
+      large += "l"
+    }
+  }
+  return small + medium + large
+}
+
+console.log(tshirtSorter("lslslmllslmssllsmmllsmlmslll"))
